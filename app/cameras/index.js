b66c465a175102d1fed05b59c6da4599a77ddeb4
Tom <tom@inoui.io>
2016-08-02 16:11:40 +0200

    cameras

---
+++ app/cameras/index.js
@@ -0,0 +1,17 @@
+// var fs = require('fs');
+// var gphoto2 = require('gphoto2');
+
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+export class Cameras {
+
+    init() {
+        
+    }
+
+    takePictures() {
+
+    }
+
+}
